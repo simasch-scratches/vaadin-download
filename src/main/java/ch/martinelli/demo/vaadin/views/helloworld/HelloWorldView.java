8ae57ed8744baf0eeab0731eeb02eed4020b464c
Simon Martinelli <simon@72.services>
2023-01-06 13:59:11 +0100

    Initial commit

--- src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
@@ -1,35 +1,37 @@
 package ch.martinelli.demo.vaadin.views.helloworld;
 
 import ch.martinelli.demo.vaadin.views.MainLayout;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.StreamResource;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class HelloWorldView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    public HelloWorldView(@Value("${pdf.file}") String filename) {
+        Anchor download = new Anchor(new StreamResource("RG-1007150-8949.pdf",
+                () -> {
+                    byte[] fileContent;
+                    try {
+                        fileContent = Files.readAllBytes(new File(filename).toPath());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return new ByteArrayInputStream(fileContent);
+                }), "Download");
+        download.getElement().setAttribute("download", true);
 
-    public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(download);
     }
-
 }
